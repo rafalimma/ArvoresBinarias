f3040adcdb5dae93c83437d03fb7a8d849ed9417
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-09-04 20:11:39 -0300

    Add files via upload

---
+++ Estruturas de Dados Algoritmos/ponteiro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a = 10;
+    int *f = &a; // p é um ponteiro para a variável a
+
+    int *p;
+    int aux = 10;
+    printf("%d %d\n", aux, p);
+}
