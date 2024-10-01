8b8d22858d678bdc0b7c1bc2015db0168799db11
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-10-01 16:33:01 -0300

    Add files via upload

---
+++ codewars/train2.c
@@ -0,0 +1,19 @@
+void fakeBin(const char *digits, char *binary) {
+  // Please place result in the memory pointed to by
+  // the binary parameter. binary has enough memory to
+  // accommodate the answer as well as the null-terminating
+  // character.
+  int i;
+  int size;
+  for(i = 0; &digits != '\0';i++) {
+    size = i + 1;
+  }
+  for(i = 0; digits > size - 1 ;i++) {
+    if (digits[i] < 5) {
+      &binary[i] = '0';
+    }else{
+      &binary[i] = '1'
+    }
+  }
+ 	*binary = '\0';
+}
