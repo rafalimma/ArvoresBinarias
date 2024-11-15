8614224015cf878a231a7c8f17226035fe801b17
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-11-15 12:19:49 -0300

    Inserindo no Grafo

--- main.c
+++ main.c
@@ -6,20 +6,33 @@
 // indica se a pessoa i é amiga da pessoa j. Se i e j são amigos, colocamos "1"; caso contrário, colocamos "0".
 
 typedef struct Grafo {
-    int amigos[MAX];
-    int adhjacencia[MAX][MAX];
+    int amigos;
+    int adjacencia[MAX][MAX];
 } Grafo;
 
 Grafo* InitGrafo(int n_amigos) {
     Grafo* g = (Grafo*)malloc(sizeof(Grafo));
     g->amigos = n_amigos;
     for (int i=0;i<MAX;i++) {
-
+        for (int j=0;j<MAX;j++) {
+            g->adjacencia[i][j] = 0;
+        }
     }
+    return g;
 }
 
-int main(void)
-{
-    printf("Hello, World!\n");
+void InsertAmizade(Grafo* g, int amigo1, int amigo2) {
+    g->adjacencia[amigo1][amigo2] = 1;
+    g->adjacencia[amigo2][amigo1] = 1;
+}
+
+int main() {
+    Grafo* grafoo_amigos = InitGrafo(MAX);
+    InsertAmizade(grafoo_amigos, 0, 2);
+    InsertAmizade(grafoo_amigos, 1, 3);
+    InsertAmizade(grafoo_amigos, 4, 5);
+
+    free(grafoo_amigos);
     return 0;
 }
+
